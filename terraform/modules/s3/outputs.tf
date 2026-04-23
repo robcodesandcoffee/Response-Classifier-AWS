b07ec2f7901d8b017edc89b9dcc3a57bbdd3d4d5
Robert <writers.40exit@icloud.com>
2026-04-23 17:39:13 +0000

    Reformatting tf files

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,7 +1,7 @@
-output "data_bucket_name"      { value = aws_s3_bucket.this["data"].bucket }
-output "data_bucket_arn"       { value = aws_s3_bucket.this["data"].arn }
+output "data_bucket_name" { value = aws_s3_bucket.this["data"].bucket }
+output "data_bucket_arn" { value = aws_s3_bucket.this["data"].arn }
 output "artifacts_bucket_name" { value = aws_s3_bucket.this["artifacts"].bucket }
-output "artifacts_bucket_arn"  { value = aws_s3_bucket.this["artifacts"].arn }
-output "models_bucket_name"    { value = aws_s3_bucket.this["models"].bucket }
-output "models_bucket_arn"     { value = aws_s3_bucket.this["models"].arn }
-output "logs_bucket_name"      { value = aws_s3_bucket.this["logs"].bucket }
+output "artifacts_bucket_arn" { value = aws_s3_bucket.this["artifacts"].arn }
+output "models_bucket_name" { value = aws_s3_bucket.this["models"].bucket }
+output "models_bucket_arn" { value = aws_s3_bucket.this["models"].arn }
+output "logs_bucket_name" { value = aws_s3_bucket.this["logs"].bucket }

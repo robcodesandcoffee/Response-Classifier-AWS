8c0b97dda25a846580952e76185e368ff622517e
Robert <writers.40exit@icloud.com>
2026-05-20 22:19:39 +0000

    Updating CDK initial files

--- cdk/stacks/inference_api_stack.py
+++ cdk/stacks/inference_api_stack.py
@@ -1,8 +1,16 @@
+from __future__ import annotations
 import aws_cdk as cdk
 from constructs import Construct
 
 
 class InferenceApiStack(cdk.Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs):
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        **kwargs,
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        # resources go here
+
+        # Resources will be added here in later steps.
+        pass
